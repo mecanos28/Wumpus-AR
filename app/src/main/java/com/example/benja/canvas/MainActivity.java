0cd6903379096e5a5be49b55a6f4d243239771a3
benja <crbenjaminblanco@gmail.com>
2017-09-28 10:35:08 -0600

    Use interface

--- app/src/main/java/com/example/benja/canvas/MainActivity.java
+++ app/src/main/java/com/example/benja/canvas/MainActivity.java
@@ -17,6 +17,17 @@ public class MainActivity extends Activity {
 
     }
 
+    public void metodoCargar(View vista)
+    {
+        setContentView(R.layout.layout_cargar);
+    }
+
+    public void metodoNuevo (View vista)
+    {
+        setContentView(R.layout.layout_nuevo);
+    }
+
+
     public void tipoIndividual(View vista)
     {
         setContentView(R.layout.layout_individual);
@@ -27,5 +38,11 @@ public class MainActivity extends Activity {
     {
         setContentView(R.layout.layout_multijugador);
     }
+
+    public void volverInicio(View vista)
+    {
+        setContentView(R.layout.activity_main);
+    }
+
 
 }
