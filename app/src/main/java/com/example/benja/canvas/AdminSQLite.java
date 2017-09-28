bbdd2f9fa5ead7210b66fb46d6eb5d2551aabee3
Carlos <cali1997gv@hotmail.com>
2017-09-28 10:31:40 -0600

    Use SQLite to store information

---
+++ app/src/main/java/com/example/benja/canvas/AdminSQLite.java
@@ -0,0 +1,48 @@
+package com.example.benja.canvas;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/*
+*   This class starts the DB.
+*/
+
+public class AdminSQLite extends SQLiteOpenHelper {
+
+    public AdminSQLite(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE GRAPH (" +
+                "id INTEGER AUTO INCREMENT PRIMARY KEY," +
+                "relations TEXT" +
+                ");");
+        db.execSQL("CREATE TABLE CAVE (" +
+                "id INTEGER AUTO INCREMENT PRIMARY KEY," +
+                "graph_id INTEGER," +
+                "cave_number INTEGER," +
+                "FOREIGN KEY (graph_id) REFERENCES GRAPH(id)" +
+                ");");
+        db.execSQL("CREATE TABLE GAME (" +
+                "id INTEGER AUTO INCREMENT PRIMARY KEY," +
+                "graph_id INTEGER," +
+                "cave_id INTEGER," +
+                "latitude TEXT," +
+                "longitude TEXT" +
+                "cave_number INTEGER," +
+                "FOREIGN KEY (graph_id) REFERENCES GRAPH(id)," +
+                "FOREIGN KEY (cave_id) REFERENCES CAVE(id)" +
+                ");");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("DROP TABLE IF EXISTS GRAPH;");
+        db.execSQL("DROP TABLE IF EXISTS CAVE;");
+        db.execSQL("DROP TABLE IF EXISTS GAME;");
+        onCreate(db);
+    }
+}
