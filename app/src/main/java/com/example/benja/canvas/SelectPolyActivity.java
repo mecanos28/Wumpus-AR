29d8e0213a293a3eba4cb0d9319f9384de2d159c
carlos-gamboa <cali1997gv@hotmail.com>
2017-09-28 21:25:31 -0600

    Obtener el laberinto regular una vez que se hace click en su imagen.

--- app/src/main/java/com/example/benja/canvas/SelectPolyActivity.java
+++ app/src/main/java/com/example/benja/canvas/SelectPolyActivity.java
@@ -1,7 +1,12 @@
 package com.example.benja.canvas;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class SelectPolyActivity extends AppCompatActivity {
 
@@ -9,5 +14,45 @@ public class SelectPolyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_poly);
+    }
+
+    /*
+    * Gets a regular maze from the DB once an image is clicked.
+    */
+    void imageClicked(View v) {
+        Button button = (Button) findViewById(v.getId());
+        String buttonID = button.getText().toString();
+        AdminSQLite admin = new AdminSQLite(this, "WumpusDB", null, 1);
+        SQLiteDatabase db = admin.getWritableDatabase();
+        String graphName = "";
+        Integer graphID;
+        switch (buttonID) {
+            case "btnTetrahedron":
+                graphName = "Tetrahedron";
+                break;
+            case "btnOctahedron":
+                graphName = "Octahedron";
+                break;
+            case "btnCube":
+                graphName = "Cube";
+                break;
+            case "btnIcosahedron":
+                graphName = "Icosahedron";
+                break;
+            case "btnDodecahedron":
+                graphName = "Dodecahedron";
+                break;
+        }
+        Cursor cell = db.rawQuery("SELECT id FROM GRAPH WHERE GRAPH.name = " + graphName, null);
+        if (cell.moveToFirst()){
+            graphID = cell.getInt(0);
+            cell.close();
+            //TODO: Call the next layout and send the id as parameter.
+        }
+        else {
+            Toast.makeText(this, "The Wumpus isn't around this caves. Try another one!", Toast.LENGTH_LONG).show();
+            db.close();
+        }
+        cell.close();
     }
 }
