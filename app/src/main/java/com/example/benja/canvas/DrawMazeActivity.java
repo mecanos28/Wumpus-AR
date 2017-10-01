a67ff9f74f0f05501c5e23da34342868d7792302
carlos-gamboa <cali1997gv@hotmail.com>
2017-10-01 14:40:55 -0600

    Almacenar imagenes de poliedros irregulares en la DB

--- app/src/main/java/com/example/benja/canvas/DrawMazeActivity.java
+++ app/src/main/java/com/example/benja/canvas/DrawMazeActivity.java
@@ -9,6 +9,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class DrawMazeActivity extends AppCompatActivity {
 
     Graph customMaze;
@@ -26,30 +29,40 @@ public class DrawMazeActivity extends AppCompatActivity {
     public void saveMaze() {
         if (customMaze.valid()) {
             //TODO: Take a screenshot of the draw and store it on the phone, then put the image name on the corresponding variable.
-            String image = "";
-            Integer graphID;
-            String relations = customMaze.arrayToString();
-            AdminSQLite admin = new AdminSQLite(this, "WumpusDB", null, 1);
-            SQLiteDatabase db = admin.getWritableDatabase();
-            ContentValues data = new ContentValues();
-            data.put("relations", relations);
-            data.put("number_of_caves", customMaze.getMaximumCaves());
-            data.put("image", image);
-            db.insert("GRAPH", null, data);
-            Cursor cell = db.rawQuery("SELECT id FROM GRAPH WHERE GRAPH.relations = " + relations, null);
-            if (cell.moveToFirst()){
-                graphID = cell.getInt(0);
+            //try {
+                //FileInputStream fis = new FileInputStream(LOCATION OF THE SCREENSHOT);
+                //byte[] image = new byte[fis.available()];
+                //fis.read(image);
+                Integer graphID;
+                String relations = customMaze.arrayToString();
+
+                AdminSQLite admin = new AdminSQLite(this, "WumpusDB", null, 1);
+                SQLiteDatabase db = admin.getWritableDatabase();
+
+                ContentValues data = new ContentValues();
+                data.put("relations", relations);
+                data.put("number_of_caves", customMaze.getMaximumCaves());
+                //data.put("image", image);
+                db.insert("GRAPH", null, data);
+
+                Cursor cell = db.rawQuery("SELECT id FROM GRAPH WHERE GRAPH.relations = " + relations, null);
+                if (cell.moveToFirst()) {
+                    graphID = cell.getInt(0);
+                    //fis.close();
+                    cell.close();
+                    db.close();
+                    //TODO: Ask if they want to keep drawing or if they want to play with the new maze.
+                    //TODO: If they want to play, send the graphID to the next Layout.
+                } else {
+                    Toast.makeText(this, "The Wumpus doesn't seem to like this caves. Try drawing another one!", Toast.LENGTH_LONG).show();
+                }
+                //fis.close();
                 cell.close();
                 db.close();
-                //TODO: Ask if they want to keep drawing or if they want to play with the new maze.
-                //TODO: If they want to play, send the graphID to the next Layout.
-            }
-            else {
-                Toast.makeText(this, "The Wumpus doesn't seem to like this caves. Try drawing another one!", Toast.LENGTH_LONG).show();
-                db.close();
-            }
-            cell.close();
-            db.close();
+            //}
+            //catch (IOException e) {
+              //  e.printStackTrace();
+            //}
         }
         else {
             Toast.makeText(this, "The Wumpus doesn't seem to like this caves. Try drawing another one!", Toast.LENGTH_LONG).show();
