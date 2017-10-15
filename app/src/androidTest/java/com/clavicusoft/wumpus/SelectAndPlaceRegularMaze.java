5d415ae66bed10e5325ddaf0b1aa7d23747f32fb
mecanos28 <31737854+mecanos28@users.noreply.github.com>
2017-10-15 16:49:44 -0600

    Pruebas Select and place from library y from regular maze

--- app/src/androidTest/java/com/clavicusoft/wumpus/SelectAndPlaceRegularMaze.java
+++ app/src/androidTest/java/com/clavicusoft/wumpus/SelectAndPlaceRegularMaze.java
@@ -6,6 +6,7 @@ import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
 
+import com.clavicusoft.wumpus.R;
 import com.clavicusoft.wumpus.Select.MainActivity;
 
 import org.junit.Rule;
@@ -13,8 +14,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.Espresso.pressBack;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.swipeLeft;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withParent;
@@ -23,48 +24,56 @@ import static org.hamcrest.Matchers.allOf;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class SelectRegularMazeTest {
+public class SelectAndPlaceRegularMaze {
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void selectRegularMazeTest() {
+    public void selectAndPlaceRegularMaze() {
         ViewInteraction button = onView(
                 allOf(withId(R.id.Individual), withText("Individual"),
                         withParent(withId(R.id.linearLayout)),
                         isDisplayed()));
         button.perform(click());
 
+        ViewInteraction viewPager = onView(
+                allOf(withId(R.id.ImageSlider), isDisplayed()));
+        viewPager.perform(swipeLeft());
+
+        ViewInteraction viewPager2 = onView(
+                allOf(withId(R.id.ImageSlider), isDisplayed()));
+        viewPager2.perform(swipeLeft());
+
+        ViewInteraction viewPager3 = onView(
+                allOf(withId(R.id.ImageSlider), isDisplayed()));
+        viewPager3.perform(swipeLeft());
+
+        ViewInteraction viewPager4 = onView(
+                allOf(withId(R.id.ImageSlider), isDisplayed()));
+        viewPager4.perform(swipeLeft());
+
         ViewInteraction button2 = onView(
                 allOf(withId(R.id.bttnStartGame), withText("Iniciar el juego"), isDisplayed()));
         button2.perform(click());
 
-        pressBack();
-
         ViewInteraction button3 = onView(
-                allOf(withId(R.id.bttnStartGame), withText("Iniciar el juego"), isDisplayed()));
+                allOf(withId(R.id.buttonMyLocation), withText("Agregar ubicación"), isDisplayed()));
         button3.perform(click());
 
-        pressBack();
 
-        ViewInteraction button4 = onView(
-                allOf(withId(R.id.bttnStartGame), withText("Iniciar el juego"), isDisplayed()));
-        button4.perform(click());
-
-        pressBack();
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         ViewInteraction button5 = onView(
-                allOf(withId(R.id.bttnStartGame), withText("Iniciar el juego"), isDisplayed()));
+                allOf(withId(R.id.bcontinuar), withText("¡Continuar!"), isDisplayed()));
         button5.perform(click());
-
-        pressBack();
-
-        ViewInteraction button6 = onView(
-                allOf(withId(R.id.bttnStartGame), withText("Iniciar el juego"), isDisplayed()));
-        button6.perform(click());
-
-        pressBack();
 
     }
 
