3cbedd7d88c8f5e90ba20e2b2818397451399e0f
analau05 <alauvargas@gmail.com>
2017-10-15 22:05:21 -0600

    Correr las pruebas SelectAndPlaceRegularMaze y SelectAndPlaceFromLibrary exitosamente.

--- app/src/androidTest/java/com/clavicusoft/wumpus/SelectAndPlaceFromLibrary.java
+++ app/src/androidTest/java/com/clavicusoft/wumpus/SelectAndPlaceFromLibrary.java
@@ -4,12 +4,10 @@ package com.clavicusoft.wumpus;
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.test.suitebuilder.annotation.LargeTest;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
 
-import com.clavicusoft.wumpus.R;
 import com.clavicusoft.wumpus.Select.MainActivity;
 
 import org.hamcrest.Description;
@@ -20,18 +18,20 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.Espresso.pressBack;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static android.support.test.espresso.action.ViewActions.replaceText;
-import static android.support.test.espresso.action.ViewActions.scrollTo;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withParent;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 
-@LargeTest
+/**
+ * In order to run this test successfully,
+ * you must grant the location permissions to the application,
+ * you have to create a maze with 2 caves with the name "prueba"
+ * and then you must modify in the code of the test the identifier of the saved maze, as indicated below.
+ */
+
 @RunWith(AndroidJUnit4.class)
 public class SelectAndPlaceFromLibrary {
 
@@ -52,7 +52,8 @@ public class SelectAndPlaceFromLibrary {
         button11.perform(click());
 
         ViewInteraction textView = onView(
-                allOf(withId(R.id.text_view_item), withText("Nombre: prueba-1508107570\nNúmero de cuevas: 2"),
+                //To run this test successfully change the numbers between '-' and '\n' to the current identifier of the maze.
+                allOf(withId(R.id.text_view_item), withText("Nombre: prueba-1508116860\nNúmero de cuevas: 2"),
                         childAtPosition(
                                 withId(R.id.listViewMazes),
                                 0),
--- app/src/androidTest/java/com/clavicusoft/wumpus/SelectAndPlaceRegularMaze.java
+++ app/src/androidTest/java/com/clavicusoft/wumpus/SelectAndPlaceRegularMaze.java
@@ -4,9 +4,7 @@ package com.clavicusoft.wumpus;
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.test.suitebuilder.annotation.LargeTest;
 
-import com.clavicusoft.wumpus.R;
 import com.clavicusoft.wumpus.Select.MainActivity;
 
 import org.junit.Rule;
@@ -22,7 +20,6 @@ import static android.support.test.espresso.matcher.ViewMatchers.withParent;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 
-@LargeTest
 @RunWith(AndroidJUnit4.class)
 public class SelectAndPlaceRegularMaze {
 
