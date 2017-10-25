ea4867d7a0230a7246dc56bcac0da5b098fc8015
mecanos28 <31737854+mecanos28@users.noreply.github.com>
2017-10-24 22:45:22 -0600

    Obligatorio dar permiso de acceso a la cámara para utilizar el app, si no no puede pasar de la primer pantalla

--- app/src/main/java/com/clavicusoft/wumpus/Select/MainActivity.java
+++ app/src/main/java/com/clavicusoft/wumpus/Select/MainActivity.java
@@ -3,6 +3,8 @@ package com.clavicusoft.wumpus.Select;
 import android.Manifest;
 import android.app.Activity;
 import android.app.ActivityOptions;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
@@ -13,6 +15,7 @@ import com.clavicusoft.wumpus.R;
 
 
 public class MainActivity extends Activity {
+    AlertDialog.Builder alert; //Alert
 
     /**
      * Sets the view once this activity starts.
@@ -21,6 +24,7 @@ public class MainActivity extends Activity {
     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        alert = new AlertDialog.Builder(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
@@ -30,6 +34,35 @@ public class MainActivity extends Activity {
 
     }
 
+
+    /**
+     * Requests the user to accept permissions for camera services if they
+     * were not previously accepted on installation.
+     *
+     * @param requestCode Application specific request code to match with a result
+     *                    reported to onRequestPermissionsResult(int, String[], int[])
+     * @param permissions The requested permissions.
+     * @param grantResults The grant results for the corresponding permissions.
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 1) {
+            if(grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //Permission accepted
+            } else {
+                //Permission denied
+                alert.setTitle("Error");
+                alert.setMessage("Para poder continuar con el juego debe permitir a Wumpus acceder a la cámara.");
+                alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                alert.show();
+            }
+        }
+    }
+
     /**
      * Starts the single player activity, and sets the animation for the transition.
      *
@@ -37,10 +70,18 @@ public class MainActivity extends Activity {
     */
     public void singlePlayer (View view)
     {
-        Intent i = new Intent(this,SelectPolyActivity.class);
-        ActivityOptions options = ActivityOptions.makeCustomAnimation(this, R.anim.slide_in_down,
-                R.anim.slide_out_down);
-        startActivity(i, options.toBundle());
+        //---This block of code ensures camera permissions are granted before launching anything else
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            //Permissions not granted.
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 1);
+        }
+        //---
+        else {
+            Intent i = new Intent(this, SelectPolyActivity.class);
+            ActivityOptions options = ActivityOptions.makeCustomAnimation(this, R.anim.slide_in_down,
+                    R.anim.slide_out_down);
+            startActivity(i, options.toBundle());
+        }
     }
 
     /**
@@ -50,9 +91,16 @@ public class MainActivity extends Activity {
      */
     public void multiPlayer (View view)
     {
-        Intent i = new Intent(this,Multiplayer.class);
-        ActivityOptions options = ActivityOptions.makeCustomAnimation(this, R.anim.slide_in_down,
-                R.anim.slide_out_down);
-        startActivity(i, options.toBundle());
+        //---This block of code ensures camera permissions are granted before launching anything else
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 1);
+        }
+        //---
+        else {
+            Intent i = new Intent(this, Multiplayer.class);
+            ActivityOptions options = ActivityOptions.makeCustomAnimation(this, R.anim.slide_in_down,
+                    R.anim.slide_out_down);
+            startActivity(i, options.toBundle());
+        }
     }
 }
